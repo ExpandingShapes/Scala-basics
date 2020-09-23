27a0a14d13a540b7bdeb62359ef9c2e19c9184d2
Alexander <lapseofreason17@gmail.com>
2020-09-23 13:13:54 +0300

    add Task13 solution

--- src/main/scala/Task13.scala
+++ src/main/scala/Task13.scala
@@ -4,5 +4,23 @@
   */
 
 object Task13 {
-  def solution(s: String): List[String] = ???
+  def solution(s: String): List[String] = {
+    def iter(s: String, segmentsLeft: Int): List[String] = {
+      //are there too few/many chars in s to be an IP address?
+      if (s.length < segmentsLeft || s.length > segmentsLeft * 3) {
+        Nil
+      } else if (segmentsLeft == 1) {
+        s :: Nil
+      } else {
+        (1 to 3)
+          .flatMap(n =>
+            iter(s.drop(n), segmentsLeft - 1)
+              .filter(_ => s.take(n).toInt <= 255)
+              .map(i => s"${s.take(n)}.$i")
+          )
+          .toList
+      }
+    }
+    iter(s, 4)
+  }
 }
