df13865286e0f6d11e564677a26d26d759e99d30
Alexander <lapseofreason17@gmail.com>
2020-09-23 10:43:20 +0300

    add Task12 solution

--- src/main/scala/Task12.scala
+++ src/main/scala/Task12.scala
@@ -1,13 +1,16 @@
 /**
- * Check if a string can be converted to a digital number.
- * Using regular expression is preferable.
- * Allowed characters are:
- * 0-9 integers
- * e exponent
- * + or - sign
- * . in a digital fraction
- */
+  * Check if a string can be converted to a digital number.
+  * Using regular expression is preferable.
+  * Allowed characters are:
+  * 0-9 integers
+  * e exponent
+  * + or - sign
+  * . in a digital fraction
+  */
 
 object Task12 {
-  def solution(s: String): Boolean = ???
+  def solution(s: String): Boolean = {
+    val pattern = "[+-]?[0-9]+(\\.[0-9]+)?([Ee][+-]?[0-9]+)?".r
+    pattern.matches(s)
+  }
 }
--- src/test/scala/Task12Test.scala
+++ src/test/scala/Task12Test.scala
@@ -3,44 +3,45 @@ import org.scalatest.flatspec.AnyFlatSpec
 class Task12Test extends AnyFlatSpec {
   "Task12" must "return true for 0" in {
     assert(Task12.solution("0"))
-    it must "return true for 0.1" in {
-      assert(Task12.solution("0.1"))
-    }
-    it must "return false for abc" in {
-      assert(!Task12.solution("abc"))
-    }
-    it must "return false for 1a" in {
-      assert(!Task12.solution("1a"))
-    }
-    it must "return true for 2e10" in {
-      assert(Task12.solution("2e10"))
-    }
-    it must "return true for -90e3" in {
-      assert(Task12.solution("-90e3"))
-    }
-    it must "return false for 1e" in {
-      assert(!Task12.solution("1e"))
-    }
-    it must "return false for e3" in {
-      assert(!Task12.solution("e3"))
-    }
-    it must "return true for 6e-1" in {
-      assert(Task12.solution("6e-1"))
-    }
-    it must "return false for 99e2.5" in {
-      assert(!Task12.solution("99e2.5"))
-    }
-    it must "return true for 53.5e93" in {
-      assert(Task12.solution("53.5e93"))
-    }
-    it must "return false for --6" in {
-      assert(!Task12.solution("--6"))
-    }
-    it must "return false for -+3" in {
-      assert(!Task12.solution("-+3"))
-    }
-    it must "return false for 95a54e53" in {
-      assert(!Task12.solution("95a54e53"))
-    }
   }
+  it must "return true for 0.1" in {
+    assert(Task12.solution("0.1"))
+  }
+  it must "return false for abc" in {
+    assert(!Task12.solution("abc"))
+  }
+  it must "return false for 1a" in {
+    assert(!Task12.solution("1a"))
+  }
+  it must "return true for 2e10" in {
+    assert(Task12.solution("2e10"))
+  }
+  it must "return true for -90e3" in {
+    assert(Task12.solution("-90e3"))
+  }
+  it must "return false for 1e" in {
+    assert(!Task12.solution("1e"))
+  }
+  it must "return false for e3" in {
+    assert(!Task12.solution("e3"))
+  }
+  it must "return true for 6e-1" in {
+    assert(Task12.solution("6e-1"))
+  }
+  it must "return false for 99e2.5" in {
+    assert(!Task12.solution("99e2.5"))
+  }
+  it must "return true for 53.5e93" in {
+    assert(Task12.solution("53.5e93"))
+  }
+  it must "return false for --6" in {
+    assert(!Task12.solution("--6"))
+  }
+  it must "return false for -+3" in {
+    assert(!Task12.solution("-+3"))
+  }
+  it must "return false for 95a54e53" in {
+    assert(!Task12.solution("95a54e53"))
+  }
+
 }
