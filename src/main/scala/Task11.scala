d6a38ea46966305514766ab8d259028881e8ea4a
Alexander <lapseofreason17@gmail.com>
2020-09-24 10:39:18 +0300

    add Task11 solution

--- src/main/scala/Task11.scala
+++ src/main/scala/Task11.scala
@@ -1,7 +1,21 @@
 /**
- * Given a set of unique integers, return all its permutations.
- */
+  * Given a set of unique integers, return all its permutations.
+  */
 
 object Task11 {
-  def solution(nums: Array[Int]): List[List[Int]] = ???
+  def solution(nums: Array[Int]): List[List[Int]] = {
+    def permutateList(list: List[Int]): List[List[Int]] =
+      list match {
+        //if there are no more elements, build a permutation
+        case List(elem) => List(List(elem))
+        case list =>
+          for {
+            i <- List.range(0, list.length)
+            p <- permutateList(
+              list.slice(0, i) ++ list.slice(i + 1, list.length)
+            )
+          } yield list(i) :: p
+      }
+    permutateList(nums.toList)
+  }
 }
